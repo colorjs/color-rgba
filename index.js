e2040344a61292cdafe6161ed9c43668f9e3673f
dy <df.creative@gmail.com>
2020-09-26 10:07:06 -0400

    Update deps, cleaup

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 
 var parse = require('color-parse')
 var hsl = require('color-space/hsl')
-var clamp = require('clamp')
 
 module.exports = function rgba (color) {
+	// template literals
+	if (Array.isArray(color) && color.raw) color = String.raw(...arguments)
+
 	var values, i, l
 
 	//attempt to parse non-array arguments
@@ -15,15 +17,15 @@ module.exports = function rgba (color) {
 	if (!parsed.space) return []
 
 	values = Array(3)
-	values[0] = clamp(parsed.values[0], 0, 255)
-	values[1] = clamp(parsed.values[1], 0, 255)
-	values[2] = clamp(parsed.values[2], 0, 255)
+	values[0] = Math.min(Math.max(parsed.values[0], 0), 255)
+	values[1] = Math.min(Math.max(parsed.values[1], 0), 255)
+	values[2] = Math.min(Math.max(parsed.values[2], 0), 255)
 
 	if (parsed.space[0] === 'h') {
 		values = hsl.rgb(values)
 	}
 
-	values.push(clamp(parsed.alpha, 0, 1))
+	values.push(Math.min(Math.max(parsed.alpha, 0), 1))
 
 	return values
 }
